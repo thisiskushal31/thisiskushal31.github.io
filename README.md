2bdde0716f51460c69873a2a597ced2ed4e167f3
thisiskushal31 <73896226+thisiskushal31@users.noreply.github.com>
2025-03-17 05:31:43 +0530

    Add initial project files including components, styles, and configuration

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# thisiskushal.github.io
+<div align="center">
+  <h1>Kushal Gupta's Github Pages Personal Portfolio 👋💻</h1>
+  <h3>Live Website : <a href="https://thisiskushal31.github.io">Click Here!</a></h3>
+</div>    
+
+<br>
+
+##  Tech Stacks
+
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)![JavaScript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)![Nodejs](https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)![NPM](https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white)
+
+## See My Portfolio Website on Local Setup
+#### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+##  License
+
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/thisiskushal31/portfolio/blob/main/LICENSE) file for details. 
+
+## Lets Connect
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/thisiskushalgupta/)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/thisis_kushal)
+[![github](https://img.shields.io/badge/github-0d1117?style=for-the-badge&logo=github&logoColor=white)](https://github.com/thisiskushal31/)
---
+++ src/portfolio.js
@@ -0,0 +1,70 @@
+const header = {
+  homepage: '/',
+  title: 'KG.',
+}
+
+const about = {
+  name: 'Kushal Gupta',
+  role: 'Full Stack Developer and Cloud Enthusiast',
+  description:
+    'I am an final year undergraduate student based in India who is passionate about developing Full-Stack Applications and is interested in DevOps and Cloud technologies. Besides that, I seek to explore a variety of cutting-edge cloud technologies in the software development industry.',
+  resume: 'https://drive.google.com/file/d/1ReneEny0T9KiNwLjUVxLw96yPcRsNmkf/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/thisiskushalgupta/',
+    github: 'https://github.com/thisiskushal31',
+    external: 'https://linktr.ee/thisiskushalgupta',
+  },
+}
+
+const projects = [
+  {
+    name: 'Agility',
+    description:
+      'Agilty is a task management tool that allows you to plan, organise, and prioritise all of your tasks so that you can complete them in the shortest amount of time possible built using ReactJS on the frontend and MongoDB on the backend.',
+    stack: ['SASS', 'ReactJS', 'MongoDB'],
+    sourceCode: 'https://github.com/thisiskushal31/Agility',
+    livePreview: 'https://youtu.be/v2W6W7GKgro',
+  },
+  {
+    name: 'SocialSplit',
+    description:
+      'SocialSplit is a real-time chat application built on the backend with NodeJS, Express, and Socket.io and the frontend with React.',
+    stack: ['ChakraUI', 'ReactJS', 'Socket.io', 'MongoDB'],
+    sourceCode: 'https://github.com/thisiskushal31/SocialSplit',
+    livePreview: 'https://youtu.be/AIHm5PXM_GA',
+  },
+  {
+    name: 'LinkSaver Chrome Extension',
+    description:
+      'The LinkSaver Chrome extension saves all of your tabs. LinkSaver uses Javascript save in LocalStorage to save all tabs in the currently active Chrome window to your bookmarks.',
+    stack: ['Javascript', 'HTML', 'CSS'],
+    sourceCode: 'https://github.com/thisiskushal31/link-saver-extension',
+    livePreview: 'https://github.com/thisiskushal31/link-saver-extension',
+  },
+]
+
+const skills = [
+  'C/C++',
+  'Java',
+  'HTML',
+  'CSS',
+  'SASS',
+  'JavaScript',
+  'React',
+  'NodeJS',
+  'ExpressJS',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'CI/CD',
+  'Docker',
+  'Kubernetes',
+  'Google Cloud Platform',
+  'AWS'
+]
+
+const contact = {
+  email: 'guptakushal070@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
