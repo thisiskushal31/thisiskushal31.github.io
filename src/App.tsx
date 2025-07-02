70d5a35fc5796bc1ae42b3fc65fdd6d91f2f5eac
thisiskushal31 <73896226+thisiskushal31@users.noreply.github.com>
2025-07-02 22:50:20 +0530

    fix: replace BrowserRouter with HashRouter for improved routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import { HashRouter } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -22,7 +22,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <div className="min-h-screen bg-background">
             <Navigation />
             <Routes>
@@ -35,7 +35,7 @@ const App = () => (
             </Routes>
             <ScrollToTop />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
